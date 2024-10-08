4b4b283823dc2660618e176fbdcb595fd08df71b
alish <alifshania222@gmail.com>
2024-10-08 05:50:13 +0330

    finishup route project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from './pages/Login.jsx'
+import Regester from './pages/Regester.jsx'
+import Home from './pages/Home.jsx'
+import NoteFaund from "./pages/NoteFaund.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function logout() {
+    localStorage.clear()
+    return <Navigate to={'/login'}/>
 }
 
-export default App
+
+function RegisterAndLogout() {
+    localStorage.clear()
+    return <Regester/>
+}
+
+export const App = () => {
+    return (
+            <Routes>
+                <Route path={'/'} element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+                <Route path={'/register'} element={<RegisterAndLogout/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'*'} element={<NoteFaund/>}></Route>
+            </Routes>
+
+    )
+}
